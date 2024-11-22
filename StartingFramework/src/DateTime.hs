ff8bad5d2953adb407c752e5604d0cb9fc622920
hugokrul <hugo.krul@gmail.com>
2024-11-22 14:18:50 +0100

    first version, 31 februari is possible

--- StartingFramework/src/DateTime.hs
+++ StartingFramework/src/DateTime.hs
@@ -4,6 +4,7 @@ import ParseLib
 import Control.Monad
 
 import Debug.Trace
+import Data.Char (isDigit)
 
 -- | "Target" datatype for the DateTime parser, i.e, the parser should produce elements of this type.
 data DateTime = DateTime { date :: Date
@@ -34,49 +35,82 @@ parseDateTime :: Parser Char DateTime
 parseDateTime = DateTime <$> parseDate <*> parseTime <*> parseUTC
 
 parseUTC :: Parser Char Bool
-parseUTC = pure True
+parseUTC = look >>= \input ->
+    case drop 15 input of
+        (x:xs) -> if x == 'Z' then pure True else pure False
 
 parseDate :: Parser Char Date
-parseDate = do
-    y <- parseYear
-    m <- parseMonth
-    d <- parseDay
-    return (Date y m d)
+parseDate = Date <$> parseYear <*> parseMonth <*> parseDay
     
     -- Date <$> parseYear <*> parseMonth <*> parseDay
 
 parseYear :: Parser Char Year
 parseYear = look >>= \input ->
-    trace ("test: " ++ input) $
-    case input of
-        (x:xs) | read [x] >= 0 && read [x] <= 9 -> pure $ Year $ read [x]
-        _ -> failp  -- Fail for invalid input
+    case take 4 input of
+        ls | checkAllDigits ls -> pure $ Year $ read $ take 4 input
+        _ -> empty  -- Fail for invalid input
 
---TODO: parse the first four digits of the input as the year.
+checkAllDigits :: String -> Bool
+checkAllDigits = all isDigit
 
 -- [4..5]
 parseMonth :: Parser Char Month
--- [6..7]   
-parseMonth = pure (Month 11)
+parseMonth = let takeMonth str = take 2 $ drop 4 str in
+    look >>= \input ->
+    case takeMonth input of
+        ls | checkMonth ls -> pure $ Month $ read $ takeMonth input
+        _ -> empty
 
+checkMonth :: String -> Bool
+checkMonth monthString = checkAllDigits monthString && smallerThenTwelve
+    where 
+        smallerThenTwelve = 12 >= read monthString
+
+-- [6..7]   
 parseDay :: Parser Char Day
-parseDay = pure (Day 21)
--- something like a parseInt to convert from int to day
+parseDay = let takeDay str = take 2 $ drop 6 str in
+    look >>= \input ->
+    case takeDay input of
+        ls | checkDay ls -> pure $ Day $ read $ takeDay input
+        _ -> empty
+
+checkDay :: String -> Bool
+checkDay dayString = checkAllDigits dayString && checkMaximumDayCount
+    where
+        checkMaximumDayCount = dayString <= "31"
+
 
 -- after T
 parseTime :: Parser Char Time
 parseTime = Time <$> parseHour <*> parseMinute <*> parseSecond
--- [0..1]
+
+checkTime :: String -> Int -> Bool
+checkTime hourString maximum = checkAllDigits hourString && checkMaximumDayCount
+    where
+        checkMaximumDayCount = hourString <= show maximum
+
 parseHour :: Parser Char Hour
-parseHour = pure (Hour 18)
--- [2..3]
+parseHour = let takeHour str = take 2 $ drop 9 str in
+    look >>= \input ->
+    case takeHour input of
+        ls | checkTime ls 24 -> pure $ Hour $ read $ takeHour input
+        _ -> empty
+
 
 parseMinute :: Parser Char Minute
-parseMinute = pure (Minute 19)
+parseMinute = let takeMinute str = take 2 $ drop 11 str in
+    look >>= \input ->
+    case takeMinute input of
+        ls | checkTime ls 60 -> pure $ Minute $ read $ takeMinute input
+        _ -> empty
 -- [4..5]
 
 parseSecond :: Parser Char Second
-parseSecond = pure (Second 00)
+parseSecond = let takeSecond str = take 2 $ drop 13 str in
+    look >>= \input ->
+    case takeSecond input of
+        ls | checkTime ls 60 -> pure $ Second $ read $ takeSecond input
+        _ -> empty
 
 -- Exercise 2
 run :: Parser a b -> [a] -> Maybe b
@@ -84,14 +118,16 @@ run :: Parser a b -> [a] -> Maybe b
 -- parse :: Parser String DateTime -> String -> [(DateTime, String)]
 run parser [] = Nothing
 run parser input = case parse parser input of
-    [(result, _)] -> Just result
+    [(result, empty)] -> Just result
     _ -> Nothing
 
 -- Exercise 3
 printDateTime :: DateTime -> String
 printDateTime (DateTime (Date (Year year) (Month month) (Day day)) (Time (Hour hour) (Minute minute) (Second second)) utc)
     =
-        show year ++ "/" ++ show month ++ "/" ++ show day ++ "\t" ++ show hour ++ ":" ++ show minute ++ ":" ++ show second
+        "\nDate: " ++ show year ++ "/" ++ show month ++ "/" ++ show day ++ 
+        "\nTime: " ++ show hour ++ ":" ++ show minute ++ ":" ++ show second ++
+        "\nUTC?: " ++ show utc
 
 -- Exercise 4
 parsePrint :: [Char] -> Maybe String
